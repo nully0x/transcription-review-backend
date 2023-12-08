7e2d5effc8812b097fda1ccffb56587e73c1a83b
Samson Olusegun <olusegunsamson@live.com>
2023-12-08 08:37:47 +0100

    refactor commit

--- api/app/controllers/webhook.controller.ts
+++ api/app/controllers/webhook.controller.ts
@@ -168,15 +168,7 @@ export async function handlePushEvent(req: Request, res: Response) {
     });
   }
 
-  const commits = pushEvent.payload.head_commit?.modified;
-  if (!commits) {
-    return res.status(500).send({
-      message: "No commits found in the push event data.",
-    });
-  }
-
-  console.log(commits);
-  res.status(200).json(commits);
+  console.log(pushEvent);
 
   // try {
   //   for (const commit of commits) {
