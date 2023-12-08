15861a1c988c0f6c6ef9f76ece973ddea1900a8c
Samson Olusegun <olusegunsamson@live.com>
2023-12-08 08:47:16 +0100

    refactor commit

--- api/app/controllers/webhook.controller.ts
+++ api/app/controllers/webhook.controller.ts
@@ -168,7 +168,7 @@ export async function handlePushEvent(req: Request, res: Response) {
     });
   }
   // res.json(pushEvent);
-  res.json(pushEvent.payload.commits);
+  res.json(JSON.stringify(pushEvent));
 
   // try {
   //   for (const commit of commits) {
